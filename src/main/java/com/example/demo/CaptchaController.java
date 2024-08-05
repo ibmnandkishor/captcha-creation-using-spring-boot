4a2ab680d7f8d8931de797451805755774090af0
Nand kishor <121294572+ibmnandkishor@users.noreply.github.com>
2024-08-05 11:13:32 +0530

    Update CaptchaController.java

--- src/main/java/com/example/demo/CaptchaController.java
+++ src/main/java/com/example/demo/CaptchaController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping; 
  
 import cn.apiclub.captcha.Captcha;
-
+ 
 @Controller
 @RequestMapping("/app")
 public class CaptchaController {
